73435d094c3bc3ed1dd9d5398685717dc03d5e9e
Sofie Matheussen <matheussensofie@gmail.com>
2015-10-22 17:02:48 +0200

    uploaden versie 1 controlelrs aanspreken enzo...

---
+++ memory/kaartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memory
+{
+    public  class KaartController
+    {
+        private KaartView view;
+        private KaartModel model;
+
+        // Constructor
+        public KaartController()
+        {
+            // Maak instanties van model en view aan
+            model = new KaartModel();
+
+            /* 
+                Injecteer huidige instante (=controller) zodat de view kan communiceren
+                wanneer er een bepaald event heeft plaatsgevonden
+            */
+            view = new KaartView(this);
+        }
+    }
+}
